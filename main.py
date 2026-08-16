e14c0b0284dec2b8c311f5bde2b0ecb6c32ea525
j-ack999 <jackwalker17501@gmail.com>
2026-08-16 18:03:12 +0100

    Finish training and change output
    output now shows the hyperparameters for that training session alongside the test and training graphs

--- main.py
+++ main.py
@@ -73,8 +73,8 @@ import matplotlib.pyplot as plt
 image_number = 55 # unlike matlab python arrays are zero indexed so this is really the image_number + 1 th image
 mnist_image = x_train[image_number, :].reshape(28,28) # take a row and all of the values in the row, now reshape it into the image
 
-fig, ax = plt.subplots()
-ax.imshow(mnist_image, cmap="grey") # the colourmap is grey meaning that we get a black background and greyscale image 
+# fig, ax = plt.subplots()
+# ax.imshow(mnist_image, cmap="grey") # the colourmap is grey meaning that we get a black background and greyscale image 
 # plt.show()
 # print("x_train matches to a y_train of: ",  y_train[image_number])
 
@@ -83,9 +83,9 @@ num_examples = 5
 seed = 147197952744
 rng = np.random.default_rng(seed)
 
-fig, axes = plt.subplots(1, num_examples)
-for sample, ax in zip(rng.choice(x_train, size=num_examples, replace=False), axes):
-    ax.imshow(sample.reshape(28, 28), cmap="gray")
+# fig, axes = plt.subplots(1, num_examples)
+# for sample, ax in zip(rng.choice(x_train, size=num_examples, replace=False), axes):
+#     ax.imshow(sample.reshape(28, 28), cmap="gray")
 # plt.show()
 
 # fig is the whole output and axes are the individual sub plots within the figure. like the randomly picked numbers 
@@ -114,8 +114,8 @@ print(training_images.shape) # 1000, 784
 print(training_images[0])
 
 
-fig, ax = plt.subplots()
-ax.imshow(training_images[0].reshape(28,28), cmap="grey")
+# fig, ax = plt.subplots()
+# ax.imshow(training_images[0].reshape(28,28), cmap="grey")
 # plt.show()
 
 # we now know the first image in the dataset looks like a 5.
@@ -198,9 +198,10 @@ def relu2deriv(output):
 
 # hyperparameters #
 
-learning_rate = 0.005
-epochs = 20 # the number of complete passes of data through the network 
-hidden_size = 100 # amount of neurones in the hidden layer of the network, not trivial, more a sweet spot 
+learning_rate = 0.005 # the learning rate determines how much the algo updates weights. too much and you overshoot the target and too little may take a very long time
+# default learning rate is 0.005
+epochs = 100 # the number of complete passes of data through the network 
+hidden_size = 125 # amount of neurones in the hidden layer of the network, not trivial, more a sweet spot 
 pixels_per_image  = 784 # amount of pixels in one image, 28 x 28 
 num_labels = 10 # number of possible labels for each image (0-9) -> used in the prediction stage of the network 
 
@@ -213,5 +214,171 @@ num_labels = 10 # number of possible labels for each image (0-9) -> used in the 
 weights_1 = 0.2 * rng.random((pixels_per_image, hidden_size)) - 0.1 # shape = (784, 100)
 weights_2 = 0.2 * rng.random((hidden_size, num_labels)) - 0.1 # shape = (100,10)
 
-# TRAINING PROCESS 
+# TRAINING PROCESS NOW 
+
+# store the training and test set losses and accurate predictions 
+
+store_training_loss = []
+store_training_accurate_pred = []
+store_test_loss = []
+store_test_accurate_pred = []
+
+# run the learning experiment for a number of epochs (iterations)
+
+for j in range(epochs):
+
+    # set initial loss and accurate prediction number to zero
+
+    training_loss = 0.0 # how bad was the networks prediction for the correct number? 
+    training_accurate_prediction = 0 # a counter for the amount of correct predictions the network makes 
+
+    for i in range(len(training_images)): 
+        # for all images in the training set, perform a forward pass and backpropagation, then adjust the weights accordingly
+
+
+        # the input layer - sets the training image data as the inputs 
+        layer_0 = training_images[i]
+
+        # the hidden layer - take the training image data into the middle layer by multiplying it by randomly set weights from earlier 
+
+        layer_1 = np.dot(layer_0, weights_1) # the dot product of the two to keep matrix multiplication rules valid 
+
+        # pass the hidden layers output through the relu activation function 
+
+        layer_1 = relu(layer_1)
+
+        # dropout function for regularisation
+
+        # regularisation prevents the neural net from overfitting. we want the neural net to get good at learning patterns which will allow it to perform well
+        # on data it hasnt seen before, and not JUST this dataset
+
+        # regularisation deliberately makes training harder or noisier, in tern forcing the nwtwork to learn general patterns instead of memorising specifics 
+        dropout_mask = rng.integers(low=0, high=2, size=layer_1.shape) # value is a coin flip, zero or one. we do this for every neuron in the hidden layer (layer1)
+
+        # apply the dropout mask to the hidden layers output 
+
+        layer_1 *= dropout_mask * 2 
+
+        # get the output layer the same way we got the hidden, use matrix multiplication with the weights 
+
+        layer_2 = np.dot(layer_1, weights_2) # produces a 10 - dimension vector with 10 scores
+
+        # backpropagation starts here: 
+
+        # measure the training error between the actual image label (ground truth) and the prediction by the model
+
+        training_loss += np.sum((training_labels[i] - layer_2) ** 2) # incriment training loss by the difference of that image label and the prediction squared 
+        # why square this? forces everything to be positive as well as punishing big errors more than small ones. eg an error of 2 becomes 4 but an error of 10 becomes 100
+        # squaring also means that the negatives and positives do not cancel each other out, giving a result that isn't meaningful 
+
+        ## Theory ##
+
+        # training_labels[i] might look like [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+        # layer_2 might look like [0.1, -0.3, 0.05, 0.4, 0.2, -0.1, 0.15, -0.05, 0.3, 0.25]
+
+        # subtracting these gives: [-0.1, 0.3, -0.05, 0.6, -0.2, 0.1, -0.15, 0.05, -0.3, -0.25]
+
+        # so our ground truth was 3, since the one hot encoding gave us a 1 in the 4th index (zero indexed therefore this corresponds to a 3)
+
+        # at the GT the network predicted 0.4, an under prediction since we shouldve scored higher than it did (ideally as close to 1 as possible)
+        # when we square the sum we get a total error, and the negatives and positives cancelling is no longer a problem
+
+        training_accurate_prediction += int(np.argmax(layer_2) == np.argmax(training_labels[i]))
+        # note that argmax returns the index of the largest number 
+        # therefore all this line of code does is incriment the training prediction counter by 1 when both values are identical, that is the training label is the same as the prediction
+        # we get a 1 or zero here because of int(). remember the int() of a boolean simply returns 1 or 0 for true and false 
+
+        layer_2_delta = training_labels[i] - layer_2
+        layer_1_delta = np.dot(weights_2, layer_2_delta) * relu2deriv(layer_1)
+
+        # apply dropout to gradients
+        layer_1_delta *= dropout_mask
+
+        # update the weights for the middle and input layers by multiplying them by the learning rate and the gradients 
+
+        weights_1 += learning_rate * np.outer(layer_0, layer_1_delta)
+        weights_2 += learning_rate * np.outer(layer_1, layer_2_delta)
+
+    # store training set losses and accurate predictions, appending this information to the previously initialised variables
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ # 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ # 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~This section is dedecated to the storing and plottting of~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ # 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ # 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ # 
+
+    store_training_loss.append(training_loss)
+    store_training_accurate_pred.append(training_accurate_prediction)
+
+    # Evaluate the model
+
+    results = relu(test_images @ weights_1) @ weights_2
+
+    # measure the error between the ground truth and the prediction value 
+    test_loss = np.sum((test_labels - results) ** 2 )
+
+    # measure the prediction accuracy on the test set 
+
+    test_accurate_predictons = np.sum(np.argmax(results, axis=1) == np.argmax(test_labels, axis=1))
+
+    # store the test set losses and accurate prediction
+
+    store_test_loss.append(test_loss)
+    store_test_accurate_pred.append(test_accurate_predictons)
+
+    # Summarize error and accuracy metrics at each epoch
+    print(
+        (
+            f"Epoch: {j}\n"
+            f"  Training set error: {training_loss / len(training_images):.3f}\n"
+            f"  Training set accuracy: {training_accurate_prediction / len(training_images)}\n"
+            f"  Test set error: {test_loss / len(test_images):.3f}\n"
+            f"  Test set accuracy: {test_accurate_predictons / len(test_images)}"
+        )
+    )
+
+epoch_range = np.arange(epochs) + 1  # Starting from 1
+
+# The training set metrics.
+training_metrics = {
+    "accuracy": np.asarray(store_training_accurate_pred) / len(training_images),
+    "error": np.asarray(store_training_loss) / len(training_images),
+}
+
+# The test set metrics.
+test_metrics = {
+    "accuracy": np.asarray(store_test_accurate_pred) / len(test_images),
+    "error": np.asarray(store_test_loss) / len(test_images),
+}
+
+hyperparameters = {
+    "learning_rate" : learning_rate,
+    "epochs" : epochs, 
+    "hidden_size" : hidden_size, 
+    "pixels_per_image"  : pixels_per_image, 
+    "num_labels" : num_labels,
+}
+
+# Display the plots.
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
+for ax, metrics, title in zip(
+    axes, (training_metrics, test_metrics), ("Training set", "Test set")
+):
+    # Plot the metrics
+    for metric, values in metrics.items():
+        ax.plot(epoch_range, values, label=metric.capitalize())
+
+        
+    ax.set_title(title)
+    ax.set_xlabel("Epochs")
+    ax.legend()
+
+
+# third plot (pure text) 
+axes[2].axis("off")
+axes[2].set_title("Hyperparameters")
+
+for i, (name, value) in enumerate(hyperparameters.items()):
+    axes[2].text(0.1, 0.9 - i * 0.15, f"{name} = {value}", transform=axes[2].transAxes)
+
+plt.show()
 
